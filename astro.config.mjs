93710d589c057c58f217538ee17dbd66b5340245
Lion2478 <thelion.cod@gmail.com>
2025-08-27 12:23:53 -0600

    Configuración para GitHub Pages y dominio personalizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lion2478.github.io',
   integrations: [],
   vite: {
     css: {
