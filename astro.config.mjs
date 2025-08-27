13c54f1ad9aace632d3e348eec33e58b0634bd4c
Lion2478 <thelion.cod@gmail.com>
2025-08-27 12:44:57 -0600

    Actualizada configuración de GitHub Pages y workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://leonardohdz.me',
+  site: 'https://lion2478.github.io',
+  base: '/',
+  outDir: './dist',
   integrations: [],
   vite: {
     css: {
